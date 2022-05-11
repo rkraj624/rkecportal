f619b5c67fe286e5ecdefbcd187b264836230d23
Ravi Kishan Raj <ravikishan624@.com>
2022-05-11 11:50:04 +0530

    commit

---
+++ build/precache-manifest.e6b58a75c235e75b48f973e62c9ac032.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fb76df32d5704f81ca4dd259a2d3e76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fdebac028ae5a7d7022",
+    "url": "/static/css/2.fe62fa80.chunk.css"
+  },
+  {
+    "revision": "76735777ffb8a3db40ed",
+    "url": "/static/css/main.e3744cff.chunk.css"
+  },
+  {
+    "revision": "6fdebac028ae5a7d7022",
+    "url": "/static/js/2.06c72cd9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.06c72cd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76735777ffb8a3db40ed",
+    "url": "/static/js/main.7d782231.chunk.js"
+  },
+  {
+    "revision": "4f6e34b3be3410815ed1",
+    "url": "/static/js/runtime-main.07b96789.js"
+  }
+]);
